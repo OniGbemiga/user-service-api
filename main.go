74edb4edee1c20d29c085de1ed9ab54e379fbbe4
Gbemiga <onigbenga76@gmail.com>
2022-04-23 10:37:18 +0100

    refactored main and database files

--- main.go
+++ main.go
@@ -1,51 +1,23 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
-	"log"
-	"net/http"
-	database "user-service"
-)
+//type User struct {
+//	gorm.Model
+//	Username *string `gorm:"unique" json:"username"`
+//	Email    *string `gorm:"unique" json:"email"`
+//	Phone    *string `gorm:"unique" json:"phone"`
+//	Password string  `gorm:"null" json:"password"`
+//}
 
-type User struct {
-	gorm.Model
-	Username *string `gorm:"unique" json:"username"`
-	Email    *string `gorm:"unique" json:"email"`
-	Phone    *string `gorm:"unique" json:"phone"`
-	Password string  `gorm:"null" json:"password"`
-}
-
-type UserInput struct {
-	Email    *string `json:"email"`
-	Phone    *string `json:"phone"`
-	Password string  `json:"password" binding:"required"`
-}
+//type UserInput struct {
+//	Email    *string `json:"email"`
+//	Phone    *string `json:"phone"`
+//	Password string  `json:"password" binding:"required"`
+//}
 
 func main() {
-	database.ConnectToDatabase()
+	//database.ConnectToDatabase()
+	ConnectToDatabase()
 	handleRequest()
 }
 
-func handleRequest() {
-	router := gin.Default()
-	router.POST("/u/sign-up", func(c *gin.Context) {
-		var useriinput UserInput
-		if err := c.ShouldBindJSON(&useriinput); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
 
-		user := User{
-			Email:    useriinput.Email,
-			Phone:    useriinput.Phone,
-			Password: useriinput.Password,
-		}
-		if err := database.ConnectToDatabase().Create(&user).Error; err != nil {
-			log.Fatalln(err)
-		}
-		c.JSON(http.StatusOK, gin.H{"data": user})
-
-	})
-	router.Run("localhost:8080")
-}
